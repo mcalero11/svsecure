c564fb48e5c1d88fb55bb2120785bb76ff7dc240
Marvin Calero <m.calero@gbh.com.do>
2024-01-22 10:53:46 -0600

    update dependencies and remove unused

--- src/components/FaqItem/FaqItem.tsx
+++ src/components/FaqItem/FaqItem.tsx
@@ -1,7 +1,3 @@
-"use client";
-import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
 type FaqItemProps = {
   question: string;
   answer: string;
@@ -9,19 +5,10 @@ type FaqItemProps = {
 
 const FaqItem = ({ question, answer }: FaqItemProps) => {
   return (
-    <Accordion className="w-full bg-transparent border-0 shadow-none">
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel-content"
-        id="panel-header"
-        className="px-0"
-      >
-        <h3 className="font-medium">{question}</h3>
-      </AccordionSummary>
-      <AccordionDetails className="py-0">
-        <p dangerouslySetInnerHTML={{ __html: answer }} />
-      </AccordionDetails>
-    </Accordion>
+    <>
+      <p>{question}</p>
+      <p>{answer}</p>
+    </>
   );
 };
 
