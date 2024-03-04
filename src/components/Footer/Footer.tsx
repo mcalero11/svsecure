d857925abbec44f576067c678ca6c29d44e4f0ef
Marvin Calero <mjcrequeno@gmail.com>
2024-03-03 18:14:29 -0600

    Add shadcn accordion component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,46 @@
 import faqs from "@/core/faqs.json";
 import { TEXTS } from "./constants";
-import FaqItem from "@/components/FaqItem";
 import Image from "next/image";
 import PaypalButton from "@/assets/paypal-button.webp";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const Footer = () => {
   return (
-    <footer className="bg-secondaryBg px-6">
-      <div className="flex flex-col md:flex-row py-6">
-        <div>
-          <h2 className="text-2xl font-medium">{TEXTS.TITLE}</h2>
-          <div className="flex flex-col mt-4">
-            {faqs.map((faq, index) => (
-              <FaqItem
-                key={index}
-                question={faq.question}
-                answer={faq.answer}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="w-full flex flex-col items-center py-6 md:py-0">
-          <h3 className="text-xl font-medium text-center">
-            {TEXTS.DONATION_TEXT_1}
-            <br />
-            {TEXTS.DONATION_TEXT_2}
-          </h3>
-          <Image
-            src={PaypalButton}
-            alt={TEXTS.DONATION_BUTTON_ALT}
-            className="mt-4"
-          />
-        </div>
+    <footer className="flex flex-col gap-8 lg:flex-row py-6 bg-secondaryBg px-6 md:px-16">
+      <div className="grow">
+        <h2 className="text-2xl font-medium">{TEXTS.TITLE}</h2>
+        <Accordion className="flex flex-col mt-4" collapsible type="single">
+          {faqs.map((faq, index) => (
+            <AccordionItem value={`value-${index}`} key={index}>
+              <AccordionTrigger className="text-lg font-medium text-left pt-3.5 pb-3">
+                <h6>{faq.question}</h6>
+              </AccordionTrigger>
+              <AccordionContent>
+                <p
+                  className="pl-2"
+                  dangerouslySetInnerHTML={{ __html: faq.answer }}
+                />
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+      <div className="flex flex-col flex-none items-center">
+        <h3 className="text-xl font-medium text-center">
+          {TEXTS.DONATION_TEXT_1}
+          <br />
+          {TEXTS.DONATION_TEXT_2}
+        </h3>
+        <Image
+          src={PaypalButton}
+          alt={TEXTS.DONATION_BUTTON_ALT}
+          className="mt-4"
+        />
       </div>
     </footer>
   );
--- src/components/ui/wave.tsx
+++ src/components/ui/wave.tsx
@@ -1,13 +1,12 @@
+import { cn } from "@/lib/utils";
+
 type WaveProps = {
-  additionalClasses: string;
+  className: string;
 };
 
-const Wave = ({ additionalClasses }: WaveProps) => {
-  const defaultClasses = "fill-primary";
-  const combinedClasses = `${defaultClasses} ${additionalClasses || ""}`;
-
+const Wave = ({ className }: WaveProps) => {
   return (
-    <svg className={combinedClasses} viewBox="0 0 1440 320">
+    <svg className={cn("fill-primary", className)} viewBox="0 0 1440 320">
       <path d="M0,128L34.3,106.7C68.6,85,137,43,206,32C274.3,21,343,43,411,85.3C480,128,549,192,617,192C685.7,192,754,128,823,122.7C891.4,117,960,171,1029,213.3C1097.1,256,1166,288,1234,272C1302.9,256,1371,192,1406,160L1440,128L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z" />
     </svg>
   );
