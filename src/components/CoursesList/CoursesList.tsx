7a3cc3633261bc4912f2720f6322dea3da98c4df
Marvin Calero <mjcrequeno@gmail.com>
2024-11-16 10:20:54 -0600

    chore: bump dependencies

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -8,19 +8,19 @@ type CoursesListProps = {
 const CoursesList = ({ courses }: CoursesListProps) => {
   const coursesList = courses as Course[];
   return (
-    <article className={"px-6 md:px-24 my-16"}>
+    <article className="px-6 md:px-24 my-16">
       {coursesList.map((course, index) => (
-        <>
+        <div key={index}>
           <Image
-            src={`/images/${course.image}.jpg`}
-            width={50}
-            height={50}
+            src={`/images/${course.image}`}
+            width={25}
+            height={25}
             alt={course.course_name}
             className={"w-full rounded-lg"}
           />
-          <h2 key={index}>{course.course_name}</h2>
+          <h2>{course.course_name}</h2>
           <p>{course.course_description}</p>
-        </>
+        </div>
       ))}
     </article>
   );
