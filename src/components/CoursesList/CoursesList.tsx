5964251f6fdb6bcf91f2af2aebac89e69dd91df3
Marvin Calero <mjcrequeno@gmail.com>
2024-11-16 12:02:43 -0600

    feat: add some styles

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -8,11 +8,11 @@ type CoursesListProps = {
 const CoursesList = ({ courses }: CoursesListProps) => {
   const coursesList = courses as Course[];
   return (
-    <article className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-6">
+    <article className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 xl:gap-6 px-6 max-w-screen-xl mx-auto">
       {coursesList.map((course, index) => (
         <div
           key={index}
-          className="flex flex-col gap-3 p-6 bg-white border border-transparent rounded-lg hover:border-secondaryBg  hover:shadow-sm"
+          className="flex flex-col items-center gap-3 p-6 bg-white border border-transparent rounded-lg hover:border-secondaryBg  hover:shadow-sm"
         >
           <Image
             src={`/images/${course.image}`}
@@ -21,7 +21,7 @@ const CoursesList = ({ courses }: CoursesListProps) => {
             alt={course.course_name}
             className={"rounded-lg"}
           />
-          <h2 className="font-bold">{course.course_name}</h2>
+          <h2 className="font-bold tracking-wide">{course.course_name}</h2>
           <p className="text-wrap">{course.course_description}</p>
         </div>
       ))}
